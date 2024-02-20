d044eb2d2200f7e9bf5628111ae0653e968d9bba
devrdn <rdninc@mail.ru>
2024-02-20 19:54:13 +0200

    edit comments

--- samples/4_arrays/sample01.js
+++ samples/4_arrays/sample01.js
@@ -1,14 +1,31 @@
+/**
+ * Функция, создающая стек.
+ * @param {Array} initialStack - Начальный стек (необязательный).
+ * @returns {Array} - Массив, содержащий стек и функции для работы с ним.
+ */
 function useStack(initialStack = []) {
     let stack = initialStack;
 
+    /**
+     * Добавляет элемент в стек.
+     * @param {*} item - Элемент для добавления в стек.
+     */
     const push = (item) => {
         stack.push(item);
     };
 
+    /**
+     * Извлекает элемент из стека.
+     * @returns {*} - Извлеченный элемент.
+     */
     const pop = () => {
         return stack.pop();
     };
 
+    /**
+     * Проверяет, пуст ли стек.
+     * @returns {boolean} - true, если стек пуст, иначе false.
+     */
     const isEmpty = () => {
         return stack.length === 0;
     };
@@ -21,8 +38,13 @@ function useStack(initialStack = []) {
     ];
 }
 
+/**
+ * Функция, разворачивающая строку при помощи стека.
+ * @param {string} inputString - Входная строка.
+ * @returns {string} - Развёрнутая строка.
+ */
 function reverseString(inputString) {
-    // Создаем стек с помощью функции useStack
+    // Создаём стек с помощью функции useStack
     const [stack, push, pop, isEmpty] = useStack();
 
     // Помещаем каждый символ строки в стек
@@ -39,5 +61,3 @@ function reverseString(inputString) {
 
     return reversedString;
 }
-
-console.log(reverseString('hello')); // Вывод: 'olleh'
