fd5e37c6ee99111691993f7511d80b56455e7384
/rdndev <rdninc@mail.ru>
2024-03-12 01:10:02 +0200

    add guid how to get from file

---
+++ lab/LI1/files/get_from_file.js
@@ -0,0 +1,22 @@
+/**
+ * В данном файле показано, как получить данные из файла transaction.json.
+ */
+
+// Вариант 1
+const transactions = require('transaction.json');
+// ...
+
+// Вариант 2
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'transaction.json');
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+
+    // В переменной `data` находится текст из transaction.json
+});
+// ...
