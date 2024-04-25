2b4d0ce63ee169b187eeaa733d15aa51f46a2c5f
Nichita Nartea <nichita.nartea@MacBook-Pro-Nichita.local>
2024-04-25 12:45:29 +0300

    add li3

--- lab/LI1/files/get_from_file.js
+++ lab/LI1/files/get_from_file.js
@@ -3,20 +3,20 @@
  */
 
 // Вариант 1
-const transactions = require('transaction.json');
+const transactions = require("transaction.json");
 // ...
 
 // Вариант 2
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const filePath = path.join(__dirname, 'transaction.json');
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:', err);
-        return;
-    }
+const filePath = path.join(__dirname, "transaction.json");
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
 
-    // В переменной `data` находится текст из transaction.json
+  // В переменной `data` находится текст из transaction.json
 });
 // ...
